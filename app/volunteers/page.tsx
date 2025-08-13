920edea3895d82fbb52008d5e4fa900500a23ddf
midedickson <dicksonmide@gmail.com>
2025-08-13 08:49:37 +0100

    choe: add volunteers

---
+++ app/volunteers/page.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+
+export default function VolunteersPage() {
+    useEffect(() => {
+        window.location.href = 'https://olivine-cup-272.notion.site/24335754e36780879196c9c1e858fc88?pvs=105';
+    }, [])
+    return (
+        <main>
+        </main>
+    );
+}
